0e87e87c0b158d8ab7d077ccc61af994bd3e5ccd
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-10-12 16:18:09 +0000

    added spinner1.js functional

---
+++ spinner1.js
@@ -0,0 +1,23 @@
+for (let i = 0; i < 5; i++) {
+
+  setTimeout(() => {
+    process.stdout.write('\r|   ');
+  }, 100 + (i * 800));
+
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, 300 + (i * 800));
+
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, 500 + (i * 800));
+
+  setTimeout(() => {
+    // Need to escape the backslash since it's a special character.
+    process.stdout.write('\r\\   '); 
+  }, 700 + (i * 800));
+
+}
+
+
+
